54193bb12f28ebcf95fc84e0d61ef8993d638274
hyeonseo <s21035@gsm.hs.kr>
2023-06-29 09:05:46 +0900

    :wrench: :: Hilt

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     id(ProjectProperties.Gradle.LIBRARY) version Versions.GRADLE apply false
     id(ProjectProperties.Gradle.KOTLIN) version Versions.KOTLIN apply false
     id(ProjectProperties.Gradle.KTLINT) version Versions.KTLINT apply false
+    id(ProjectProperties.Gradle.HILT_PLUGIN) version Versions.HILT apply false
 }
