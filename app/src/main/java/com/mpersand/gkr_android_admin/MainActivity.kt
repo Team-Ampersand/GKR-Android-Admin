65aaadf9668b7fa3b15a55b921df40305a5fb8e6
hyeonseo <s21035@gsm.hs.kr>
2023-06-29 15:17:40 +0900

    :sparkles: :: MainActivity

---
+++ app/src/main/java/com/mpersand/gkr_android_admin/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.mpersand.gkr_android_admin
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface(modifier = Modifier.fillMaxSize()) {
+
+            }
+        }
+    }
+}
