012283c019b092c2784ba4c4071bf8beaec55bf8
hyeonseo <s21035@gsm.hs.kr>
2023-07-03 17:12:51 +0900

    :sparkles: :: UiState

--- presentation/src/main/java/com/mpersand/presentation/viewmodel/AuthViewModel.kt
+++ presentation/src/main/java/com/mpersand/presentation/viewmodel/AuthViewModel.kt
@@ -1,11 +1,13 @@
 package com.mpersand.presentation.viewmodel
 
-import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mpersand.domain.model.auth.request.SignInRequestModel
 import com.mpersand.domain.usecase.auth.SaveTokenUseCase
 import com.mpersand.domain.usecase.auth.SignInUseCase
+import com.mpersand.presentation.viewmodel.util.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -15,19 +17,26 @@ class AuthViewModel @Inject constructor(
     private val signInUseCase: SignInUseCase,
     private val saveTokenUseCase: SaveTokenUseCase
 ): ViewModel() {
+    private val _uiState = MutableLiveData<UiState<Nothing>>()
+    val uiState: LiveData<UiState<Nothing>> = _uiState
+
     fun signIn(code: String) {
         viewModelScope.launch {
             signInUseCase(SignInRequestModel(code = code))
                 .onSuccess {
-                    Log.d("signIn", it.toString())
                     saveTokenUseCase(
                         accessToken = it.accessToken,
                         refreshToken = it.refreshToken,
                         accessTokenExp = it.accessTokenExp,
                         refreshTokenExp = it.refreshTokenExp
                     )
+                    _uiState.value = UiState.Success()
                 }.onFailure {
-                    Log.d("signIn", it.message.toString())
+                    it.exceptionHandling(
+                        badRequestAction = {
+                            _uiState.value = UiState.BadRequest
+                        }
+                    )
                 }
         }
     }
