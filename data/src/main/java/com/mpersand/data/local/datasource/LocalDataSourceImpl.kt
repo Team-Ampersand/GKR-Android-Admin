1e127f46b4c33d6e5568303bc5fca9e374ac3e7a
hyeonseo <s21035@gsm.hs.kr>
2023-07-03 11:53:16 +0900

    :sparkles: :: LocalDataSource

---
+++ data/src/main/java/com/mpersand/data/local/datasource/LocalDataSourceImpl.kt
@@ -0,0 +1,42 @@
+package com.mpersand.data.local.datasource
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "token")
+
+class LocalDataSourceImpl @Inject constructor(
+    @ApplicationContext private val context: Context
+): LocalDataSource {
+    companion object {
+        private val ACCESS_TOKEN = stringPreferencesKey("access_token")
+        private val REFRESH_TOKEN = stringPreferencesKey("refresh_token")
+        private val ACCESS_TOKEN_EXP = stringPreferencesKey("access_token_exp")
+        private val REFRESH_TOKEN_EXP = stringPreferencesKey("refresh_token_exp")
+    }
+
+    override suspend fun saveToken(accessToken: String, refreshToken: String, accessTokenExp: String, refreshTokenExp: String) {
+        context.dataStore.edit {
+            it[ACCESS_TOKEN] = accessToken
+            it[REFRESH_TOKEN] = refreshToken
+            it[ACCESS_TOKEN_EXP] = accessTokenExp
+            it[REFRESH_TOKEN_EXP] = refreshTokenExp
+        }
+    }
+
+    override suspend fun getAccessToken(): Flow<String> = context.dataStore.data.map { it[ACCESS_TOKEN] ?: "" }
+
+    override suspend fun getRefreshToken(): Flow<String> = context.dataStore.data.map { it[REFRESH_TOKEN] ?: "" }
+
+    override suspend fun getAccessTokenExp(): Flow<String> = context.dataStore.data.map { it[ACCESS_TOKEN_EXP] ?: "" }
+
+    override suspend fun getRefreshTokenExp(): Flow<String> = context.dataStore.data.map { it[REFRESH_TOKEN_EXP] ?: "" }
+}
