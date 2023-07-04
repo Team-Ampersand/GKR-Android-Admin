6d18e1b35aff232500a35d85c51970a8f11fb183
hyeonseo <s21035@gsm.hs.kr>
2023-07-04 14:42:41 +0900

    :sparkles: :: EquipmentDataSource

--- di/src/main/java/com/mpersand/di/modules/RemoteDataSourceModule.kt
+++ di/src/main/java/com/mpersand/di/modules/RemoteDataSourceModule.kt
@@ -2,6 +2,8 @@ package com.mpersand.di.modules
 
 import com.mpersand.data.remote.datasource.auth.AuthDataSource
 import com.mpersand.data.remote.datasource.auth.AuthDataSourceImpl
+import com.mpersand.data.remote.datasource.equipment.EquipmentDataSource
+import com.mpersand.data.remote.datasource.equipment.EquipmentDataSourceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -14,4 +16,9 @@ interface RemoteDataSourceModule {
     fun bindsAuthDataSource(
         authDataSourceImpl: AuthDataSourceImpl
     ): AuthDataSource
+
+    @Binds
+    fun bindsEquipmentDataSource(
+        equipmentDataSourceImpl: EquipmentDataSourceImpl
+    ): EquipmentDataSource
 }
