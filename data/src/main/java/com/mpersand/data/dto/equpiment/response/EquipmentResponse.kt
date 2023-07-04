36813ec4353756a602c61bb5b273bbcb36426b20
hyeonseo <s21035@gsm.hs.kr>
2023-07-04 15:02:05 +0900

    :sparkles: :: EquipmentRepository

--- data/src/main/java/com/mpersand/data/dto/equpiment/response/EquipmentResponse.kt
+++ data/src/main/java/com/mpersand/data/dto/equpiment/response/EquipmentResponse.kt
@@ -1,9 +1,19 @@
 package com.mpersand.data.dto.equpiment.response
 
+import com.mpersand.domain.model.equipment.response.EquipmentResponseModel
+
 data class EquipmentResponse(
     val productNumber: String,
     val name: String,
     val image: String,
     val description: String,
     val rentStatus: String
+)
+
+fun EquipmentResponse.asEquipmentResponseModel() = EquipmentResponseModel(
+    productNumber = productNumber,
+    name = name,
+    image = image,
+    description = description,
+    rentStatus = rentStatus
 )
