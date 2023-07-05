929b7178f0f7b8a7334eb61ac0378cf0a1aef98b
박성현 <pjhomg5530@naver.com>
2023-07-05 19:44:20 +0900

    :sparkles: :: di 세팅

--- di/src/main/java/com/mpersand/di/modules/RemoteDataSourceModule.kt
+++ di/src/main/java/com/mpersand/di/modules/RemoteDataSourceModule.kt
@@ -4,8 +4,12 @@ import com.mpersand.data.remote.datasource.auth.AuthDataSource
 import com.mpersand.data.remote.datasource.auth.AuthDataSourceImpl
 import com.mpersand.data.remote.datasource.equipment.EquipmentDataSource
 import com.mpersand.data.remote.datasource.equipment.EquipmentDataSourceImpl
+import com.mpersand.data.remote.datasource.order.OrderDataSource
+import com.mpersand.data.remote.datasource.order.OrderDataSourceImpl
 import com.mpersand.data.remote.datasource.repair.RepairDataSource
 import com.mpersand.data.remote.datasource.repair.RepairDataSourceImpl
+import com.mpersand.data.remote.datasource.violation.ViolationDataSource
+import com.mpersand.data.remote.datasource.violation.ViolationDataSourceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -28,4 +32,14 @@ interface RemoteDataSourceModule {
     fun bindsRepairDataSource(
         repairDataSourceImpl: RepairDataSourceImpl
     ): RepairDataSource
+
+    @Binds
+    fun bindsOrderDataSource(
+        orderDataSourceImpl: OrderDataSourceImpl
+    ): OrderDataSource
+
+    @Binds
+    fun bindsViolationDataSource(
+        violationDataSourceImpl: ViolationDataSourceImpl
+    ): ViolationDataSource
 }
