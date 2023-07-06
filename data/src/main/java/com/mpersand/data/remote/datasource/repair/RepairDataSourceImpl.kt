ce9ebb292d86b595ff176827fc6c7ff69d527c09
hyeonseo <s21035@gsm.hs.kr>
2023-07-06 09:41:36 +0900

    :sparkles: :: safeApiCall

--- data/src/main/java/com/mpersand/data/remote/datasource/repair/RepairDataSourceImpl.kt
+++ data/src/main/java/com/mpersand/data/remote/datasource/repair/RepairDataSourceImpl.kt
@@ -2,10 +2,11 @@ package com.mpersand.data.remote.datasource.repair
 
 import com.mpersand.data.dto.repair.response.RepairResponse
 import com.mpersand.data.network.api.RepairApi
+import com.mpersand.data.remote.util.safeApiCall
 import javax.inject.Inject
 
 class RepairDataSourceImpl @Inject constructor(
     private val repairApi: RepairApi
 ): RepairDataSource {
-    override suspend fun getRepairHistory(productNumber: String): List<RepairResponse> = repairApi.getRepairHistory(productNumber)
+    override suspend fun getRepairHistory(productNumber: String): List<RepairResponse> = safeApiCall { repairApi.getRepairHistory(productNumber) }
 }
