eea37c62161e4ebcff4e7760e5c88e3fb8a54765
hyeonseo <s21035@gsm.hs.kr>
2023-07-06 15:30:21 +0900

    :sparkles: :: 기자재 수정 remote 로직

--- data/src/main/java/com/mpersand/data/network/api/RepairApi.kt
+++ data/src/main/java/com/mpersand/data/network/api/RepairApi.kt
@@ -1,7 +1,10 @@
 package com.mpersand.data.network.api
 
+import com.mpersand.data.dto.repair.request.RepairRequest
 import com.mpersand.data.dto.repair.response.RepairResponse
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.Query
 
 interface RepairApi {
@@ -9,4 +12,9 @@ interface RepairApi {
     suspend fun getRepairHistory(
         @Query("productNumber") productNumber: String
     ): List<RepairResponse>
+
+    @PATCH("repair")
+    suspend fun modifyRepairHistory(
+        @Body body: RepairRequest
+    )
 }
