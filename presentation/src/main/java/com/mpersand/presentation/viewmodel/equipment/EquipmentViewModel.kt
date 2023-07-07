e0e760e9d535a4a5e0aa3d7dbfa7f947df23f1d9
hyeonseo <s21035@gsm.hs.kr>
2023-07-07 14:55:05 +0900

    :sparkles: :: EquipmentViewModel

---
+++ presentation/src/main/java/com/mpersand/presentation/viewmodel/equipment/EquipmentViewModel.kt
@@ -0,0 +1,59 @@
+package com.mpersand.presentation.viewmodel.equipment
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mpersand.domain.model.equipment.request.EquipmentRequestModel
+import com.mpersand.domain.model.equipment.response.EquipmentResponseModel
+import com.mpersand.domain.usecase.equipment.GetEquipmentDetailUseCase
+import com.mpersand.domain.usecase.equipment.ModifyEquipmentUseCase
+import com.mpersand.presentation.viewmodel.util.UiState
+import com.mpersand.presentation.viewmodel.util.exceptionHandling
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class EquipmentViewModel @Inject constructor(
+    private val modifyEquipmentUseCase: ModifyEquipmentUseCase,
+    private val getEquipmentDetailUseCase: GetEquipmentDetailUseCase
+) : ViewModel() {
+    private val _equipmentState = MutableLiveData<UiState<EquipmentResponseModel>>()
+    val equipmentState: LiveData<UiState<EquipmentResponseModel>> = _equipmentState
+
+    private val _modifyState = MutableLiveData<UiState<Nothing>>()
+    val modifyState: LiveData<UiState<Nothing>> = _modifyState
+
+    fun modifyEquipment(productNumber: String, body: EquipmentRequestModel) {
+        viewModelScope.launch {
+            modifyEquipmentUseCase(productNumber, body)
+                .onSuccess {
+                    _modifyState.value = UiState.Success()
+                }.onFailure {
+                    it.exceptionHandling(
+                        noContentAction = { _modifyState.value = UiState.NoContent },
+                        badRequestAction = { _modifyState.value = UiState.BadRequest },
+                        unauthorizedAction = { _modifyState.value = UiState.Unauthorized },
+                        forbiddenAction = { _modifyState.value = UiState.Forbidden },
+                        notFoundAction = { _modifyState.value = UiState.NotFound }
+                    )
+                }
+        }
+    }
+
+    fun getEquipmentDetail(productNumber: String) {
+        viewModelScope.launch {
+            getEquipmentDetailUseCase(productNumber)
+                .onSuccess {
+                    _equipmentState.value = UiState.Success(it)
+                }.onFailure {
+                    it.exceptionHandling(
+                        badRequestAction = { _equipmentState.value = UiState.BadRequest },
+                        unauthorizedAction = { _equipmentState.value = UiState.Unauthorized },
+                        notFoundAction = { _equipmentState.value = UiState.NotFound }
+                    )
+                }
+        }
+    }
+}
