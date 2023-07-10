d10d2659d8dd387cb64f7187e62886a79b015c0e
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-07-10 14:24:15 +0900

    :lipstick: :: RequestDetailScreen 퍼블리싱

--- presentation/src/main/java/com/mpersand/presentation/view/request/RequestDetailScreen.kt
+++ presentation/src/main/java/com/mpersand/presentation/view/request/RequestDetailScreen.kt
@@ -1,8 +1,140 @@
 package com.mpersand.presentation.view.request
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.mpersand.presentation.R
 
 @Composable
 fun RequestDetailScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Image(
+            modifier = Modifier.fillMaxWidth(),
+            painter = painterResource(id = R.drawable.temp_equipment_image),
+            contentDescription = "request detail equipment image",
+            contentScale = ContentScale.FillWidth
+        )
 
+        StudentInfo()
+
+        ReasonView()
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        AcceptOrNotView(
+            acceptButtonClick = {},
+            rejectButtonClick = {}
+        )
+    }
+}
+
+@Composable
+fun StudentInfo() {
+    Text(
+        modifier = Modifier.padding(start = 13.dp),
+        text = "[대여 요청] 오유찬 학생",
+        style = TextStyle(
+            fontFamily = FontFamily(Font(R.font.inter_black)),
+            fontSize = 20.sp
+        ),
+        softWrap = true
+    )
+
+    Text(
+        modifier = Modifier.padding(start = 13.dp),
+        text = "3학년 2반 13번",
+        style = TextStyle(
+            fontFamily = FontFamily(Font(R.font.fraunces_black)),
+            fontSize = 15.sp,
+            color = Color(0xFFC2C2C2)
+        )
+    )
+}
+
+@Composable
+fun ReasonView() {
+    Text(
+        modifier = Modifier.padding(start = 13.dp, top = 20.dp),
+        text = "연장 이유",
+        style = TextStyle(
+            fontFamily = FontFamily(Font(R.font.inter_black)),
+            fontSize = 16.sp,
+            color = Color(0xFF1C1C1E)
+        )
+    )
+
+    Text(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 13.dp),
+        text = "대여 사유",
+        style = TextStyle(
+            fontFamily = FontFamily(Font(R.font.inter_black)),
+            fontSize = 16.sp,
+            color = Color(0xFFC2C2C2)
+        ),
+        softWrap = true
+    )
+}
+
+@Composable
+fun AcceptOrNotView(
+    acceptButtonClick: () -> Unit,
+    rejectButtonClick: () -> Unit
+) {
+    Button(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(13.dp)
+            .border(width = 1.dp, color = Color(0xFF865DFF), shape = RoundedCornerShape(10.dp)),
+        shape = RoundedCornerShape(10.dp),
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+        onClick = rejectButtonClick
+    ) {
+        Text(
+            text = "거절하기",
+            style = TextStyle(
+                fontFamily = FontFamily(Font(resId = R.font.inter_light, weight = FontWeight.Medium)),
+                fontSize = 16.sp,
+                color = Color.Black
+            )
+        )
+    }
+
+    Button(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(13.dp),
+        shape = RoundedCornerShape(10.dp),
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF865DFF)),
+        onClick = acceptButtonClick
+    ) {
+        Text(
+            text = "수락하기",
+            style = TextStyle(
+                fontFamily = FontFamily(Font(resId = R.font.inter_light, weight = FontWeight.Medium)),
+                fontSize = 16.sp,
+                color = Color.White
+            )
+        )
+    }
 }
