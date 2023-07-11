554688559b35049dbbbd03c10d76bc4273d76563
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-07-11 17:30:44 +0900

    :sparkles: :: OrderRepository wait list 로직 생성

--- domain/src/main/java/com/mpersand/domain/repository/OrderRepository.kt
+++ domain/src/main/java/com/mpersand/domain/repository/OrderRepository.kt
@@ -4,4 +4,6 @@ import com.mpersand.domain.model.order.response.OrderResponseModel
 
 interface OrderRepository {
     suspend fun getNoReturnStudents(): List<OrderResponseModel>
+
+    suspend fun getWaitList(): List<OrderResponseModel>
 }
