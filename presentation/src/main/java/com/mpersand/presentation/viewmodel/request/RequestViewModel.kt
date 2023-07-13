5685cb575646a94414e05c38e54d5e56b8663629
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-07-14 01:37:05 +0900

    :sparkles: :: 대여한 기자재 list viewModel logic 생성

--- presentation/src/main/java/com/mpersand/presentation/viewmodel/request/RequestViewModel.kt
+++ presentation/src/main/java/com/mpersand/presentation/viewmodel/request/RequestViewModel.kt
@@ -4,8 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.mpersand.domain.model.order.request.OrderRequestModel
 import com.mpersand.domain.model.order.response.WaitListResponseModel
 import com.mpersand.domain.usecase.order.GetWaitListUseCase
+import com.mpersand.domain.usecase.order.RequestResultUseCase
 import com.mpersand.presentation.viewmodel.util.UiState
 import com.mpersand.presentation.viewmodel.util.exceptionHandling
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -14,11 +16,15 @@ import javax.inject.Inject
 
 @HiltViewModel
 class RequestViewModel @Inject constructor(
-    private val getWaitListUseCase: GetWaitListUseCase
+    private val getWaitListUseCase: GetWaitListUseCase,
+    private val requestResultUseCase: RequestResultUseCase
 ): ViewModel() {
 
     private val _waitList: MutableLiveData<UiState<List<WaitListResponseModel>>> = MutableLiveData()
     val waitList: LiveData<UiState<List<WaitListResponseModel>>> = _waitList
+
+    private val _requestResult = MutableLiveData<UiState<Nothing>>(UiState.Loading)
+    val requestResult: LiveData<UiState<Nothing>> = _requestResult
 
     fun getWaitList() {
         viewModelScope.launch {
@@ -29,6 +35,20 @@ class RequestViewModel @Inject constructor(
                     badRequestAction = { _waitList.value = UiState.BadRequest },
                     unauthorizedAction = { _waitList.value = UiState.Unauthorized },
                     notFoundAction = { _waitList.value = UiState.NotFound }
+                )
+            }
+        }
+    }
+
+    fun requestResult(body: OrderRequestModel) {
+        viewModelScope.launch {
+            requestResultUseCase(body).onSuccess {
+                _requestResult.value = UiState.Success()
+            }.onFailure {
+                it.exceptionHandling(
+                    badRequestAction = { _requestResult.value = UiState.BadRequest },
+                    unauthorizedAction = { _requestResult.value = UiState.Unauthorized },
+                    notFoundAction = { _requestResult.value = UiState.NotFound }
                 )
             }
         }
