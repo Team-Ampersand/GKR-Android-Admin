677219d0052554c55cdd9749ed0fb1162afb6f7c
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-07-14 01:35:36 +0900

    :sparkles: :: 대여한 기자재 list dataSource 생성

--- data/src/main/java/com/mpersand/data/remote/datasource/order/OrderDataSource.kt
+++ data/src/main/java/com/mpersand/data/remote/datasource/order/OrderDataSource.kt
@@ -1,5 +1,6 @@
 package com.mpersand.data.remote.datasource.order
 
+import com.mpersand.data.dto.order.request.OrderRequest
 import com.mpersand.data.dto.order.response.OrderResponse
 import com.mpersand.data.dto.order.response.WaitListResponse
 
@@ -7,4 +8,6 @@ interface OrderDataSource {
     suspend fun getNoReturnStudents(): List<OrderResponse>
 
     suspend fun getWaitList(): List<WaitListResponse>
+
+    suspend fun requestResult(body: OrderRequest)
 }
