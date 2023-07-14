9359450f3d1edf2a6fe549e8abb6c48347ebbc94
임가람 <84944117+ImGaram@users.noreply.github.com>
2023-07-14 11:03:17 +0900

    :recycle: :: RequestDetailScreen navigationBarsPadding 적용하기

--- presentation/src/main/java/com/mpersand/presentation/view/request/RequestDetailScreen.kt
+++ presentation/src/main/java/com/mpersand/presentation/view/request/RequestDetailScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -43,6 +44,7 @@ fun RequestDetailScreen(
         modifier = Modifier
             .fillMaxSize()
             .systemBarsPadding()
+            .navigationBarsPadding()
     ) {
         Image(
             modifier = Modifier.fillMaxWidth(),
