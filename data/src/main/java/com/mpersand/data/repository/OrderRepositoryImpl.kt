8920edbe602e8cdab48da6da97ecc1c14536977a
Cjsghkd <pjhomg5530@naver.com>
2023-10-23 11:17:39 +0900

    :recycle: 바뀐 api에 맞춰서 repository 수정

--- data/src/main/java/com/mpersand/data/repository/OrderRepositoryImpl.kt
+++ data/src/main/java/com/mpersand/data/repository/OrderRepositoryImpl.kt
@@ -1,10 +1,14 @@
 package com.mpersand.data.repository
 
 import com.mpersand.data.dto.order.request.asOrderRequest
+import com.mpersand.data.dto.order.response.asOrderDetailListResponseModel
+import com.mpersand.data.dto.order.response.asOrderListResponseModel
 import com.mpersand.data.dto.order.response.asOrderResponseModel
 import com.mpersand.data.dto.order.response.asWaitListResponseModel
 import com.mpersand.data.remote.datasource.order.OrderDataSource
 import com.mpersand.domain.model.order.request.OrderRequestModel
+import com.mpersand.domain.model.order.response.OrderDetailListResponseModel
+import com.mpersand.domain.model.order.response.OrderListResponseModel
 import com.mpersand.domain.model.order.response.OrderResponseModel
 import com.mpersand.domain.model.order.response.WaitListResponseModel
 import com.mpersand.domain.repository.OrderRepository
@@ -13,12 +17,34 @@ import javax.inject.Inject
 class OrderRepositoryImpl @Inject constructor(
     private val orderDataSource: OrderDataSource
 ): OrderRepository {
-    override suspend fun getNoReturnStudents(): List<OrderResponseModel> =
-        orderDataSource.getNoReturnStudents().map { it.asOrderResponseModel() }
+    override suspend fun getSelfStateList(): List<OrderListResponseModel> =
+        orderDataSource.getSelfStateList().map { it.asOrderListResponseModel() }
 
-    override suspend fun getWaitList(): List<WaitListResponseModel> =
-        orderDataSource.getWaitList().map { it.asWaitListResponseModel() }
+    override suspend fun getNowRentalList(): List<OrderDetailListResponseModel> =
+        orderDataSource.getNowRentalList().map { it.asOrderDetailListResponseModel() }
 
-    override suspend fun requestResult(body: OrderRequestModel) =
-        orderDataSource.requestResult(body.asOrderRequest())
+    override suspend fun getNoReturnList(): List<OrderDetailListResponseModel> =
+        orderDataSource.getNoReturnList().map { it.asOrderDetailListResponseModel() }
+
+    override suspend fun getWaitList(): List<OrderDetailListResponseModel> =
+        orderDataSource.getWaitList().map { it.asOrderDetailListResponseModel() }
+
+    override suspend fun postRental(id: Int, response: String) =
+        orderDataSource.postRental(id, response)
+
+    override suspend fun postReturn(id: Int) =
+        orderDataSource.postReturn(id)
+
+    override suspend fun postExtension(id: Int, response: String) =
+        orderDataSource.postExtension(id, response)
+
+    override suspend fun postRentalCancel(id: Int) =
+        orderDataSource.postRentalCancel(id)
+
+    override suspend fun acceptRequest(id: Int) =
+        orderDataSource.acceptRequest(id)
+
+    override suspend fun rejectRequest(id: Int) =
+        orderDataSource.rejectRequest(id)
+
 }
--- domain/src/main/java/com/mpersand/domain/repository/OrderRepository.kt
+++ domain/src/main/java/com/mpersand/domain/repository/OrderRepository.kt
@@ -1,13 +1,29 @@
 package com.mpersand.domain.repository
 
 import com.mpersand.domain.model.order.request.OrderRequestModel
+import com.mpersand.domain.model.order.response.OrderDetailListResponseModel
+import com.mpersand.domain.model.order.response.OrderListResponseModel
 import com.mpersand.domain.model.order.response.OrderResponseModel
 import com.mpersand.domain.model.order.response.WaitListResponseModel
 
 interface OrderRepository {
-    suspend fun getNoReturnStudents(): List<OrderResponseModel>
+    suspend fun getSelfStateList(): List<OrderListResponseModel>
 
-    suspend fun getWaitList(): List<WaitListResponseModel>
+    suspend fun getNowRentalList(): List<OrderDetailListResponseModel>
 
-    suspend fun requestResult(body: OrderRequestModel)
+    suspend fun getNoReturnList(): List<OrderDetailListResponseModel>
+
+    suspend fun getWaitList(): List<OrderDetailListResponseModel>
+
+    suspend fun postRental(id: Int, response: String)
+
+    suspend fun postReturn(id: Int)
+
+    suspend fun postExtension(id: Int, response: String)
+
+    suspend fun postRentalCancel(id: Int)
+
+    suspend fun acceptRequest(id: Int)
+
+    suspend fun rejectRequest(id: Int)
 }
