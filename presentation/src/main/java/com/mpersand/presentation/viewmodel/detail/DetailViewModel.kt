3821fe2ffd00ef7c640f64fed94feaa0885fde43
hyeonseo <s21035@gsm.hs.kr>
2023-10-23 11:28:25 +0900

    :sparkles: :: Repair 로직을 UseCase에 맞게 변경

--- presentation/src/main/java/com/mpersand/presentation/viewmodel/detail/DetailViewModel.kt
+++ presentation/src/main/java/com/mpersand/presentation/viewmodel/detail/DetailViewModel.kt
@@ -5,11 +5,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mpersand.domain.model.equipment.response.EquipmentResponseModel
-import com.mpersand.domain.model.repair.response.RepairResponseModel
 import com.mpersand.domain.usecase.equipment.GetEquipmentDetailUseCase
-import com.mpersand.domain.usecase.repair.GetRepairHistoryUseCase
 import com.mpersand.presentation.viewmodel.util.UiState
-import com.mpersand.presentation.viewmodel.util.combineResults
 import com.mpersand.presentation.viewmodel.util.exceptionHandling
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -18,25 +15,22 @@ import javax.inject.Inject
 @HiltViewModel
 class DetailViewModel @Inject constructor(
     private val getEquipmentDetailUseCase: GetEquipmentDetailUseCase,
-    private val getRepairHistoryUseCase: GetRepairHistoryUseCase
 ) : ViewModel() {
-    private val _uiState = MutableLiveData<UiState<Pair<EquipmentResponseModel, List<RepairResponseModel>>>>()
-    val uiState: LiveData<UiState<Pair<EquipmentResponseModel, List<RepairResponseModel>>>> = _uiState
+    private val _uiState = MutableLiveData<UiState<EquipmentResponseModel>>()
+    val uiState: LiveData<UiState<EquipmentResponseModel>> = _uiState
 
     fun getEquipmentInfo(equipmentNumber: String) {
         viewModelScope.launch {
-            combineResults(
-                { getEquipmentDetailUseCase(equipmentNumber) },
-                { getRepairHistoryUseCase(equipmentNumber) }
-            ).onSuccess {
-                _uiState.value = UiState.Success(it)
-            }.onFailure {
-                it.exceptionHandling(
-                    unauthorizedAction = { _uiState.value = UiState.Unauthorized },
-                    notFoundAction = { _uiState.value = UiState.NotFound },
-                    serverAction = { _uiState.value = UiState.Server }
-                )
-            }
+            getEquipmentDetailUseCase(equipmentNumber)
+                .onSuccess {
+                    _uiState.value = UiState.Success(it)
+                }.onFailure {
+                    it.exceptionHandling(
+                        unauthorizedAction = { _uiState.value = UiState.Unauthorized },
+                        notFoundAction = { _uiState.value = UiState.NotFound },
+                        serverAction = { _uiState.value = UiState.Server }
+                    )
+                }
         }
     }
 }
