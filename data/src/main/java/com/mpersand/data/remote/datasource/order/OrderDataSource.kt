8d000e10e337d48b08308c2e2a5d543f445db87b
Cjsghkd <pjhomg5530@naver.com>
2023-10-23 11:17:29 +0900

    :recycle: 바뀐 api에 맞춰서 datasource 수정

--- data/src/main/java/com/mpersand/data/remote/datasource/order/OrderDataSource.kt
+++ data/src/main/java/com/mpersand/data/remote/datasource/order/OrderDataSource.kt
@@ -1,13 +1,34 @@
 package com.mpersand.data.remote.datasource.order
 
 import com.mpersand.data.dto.order.request.OrderRequest
+import com.mpersand.data.dto.order.response.OrderDetailListResponse
+import com.mpersand.data.dto.order.response.OrderListResponse
 import com.mpersand.data.dto.order.response.OrderResponse
 import com.mpersand.data.dto.order.response.WaitListResponse
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface OrderDataSource {
-    suspend fun getNoReturnStudents(): List<OrderResponse>
+    suspend fun getSelfStateList(): List<OrderListResponse>
 
-    suspend fun getWaitList(): List<WaitListResponse>
+    suspend fun getNowRentalList(): List<OrderDetailListResponse>
 
-    suspend fun requestResult(body: OrderRequest)
+    suspend fun getNoReturnList(): List<OrderDetailListResponse>
+
+    suspend fun getWaitList(): List<OrderDetailListResponse>
+
+    suspend fun postRental(id: Int, response: String)
+
+    suspend fun postReturn(id: Int)
+
+    suspend fun postExtension(id: Int, response: String)
+
+    suspend fun postRentalCancel(id: Int)
+
+    suspend fun acceptRequest(id: Int)
+
+    suspend fun rejectRequest(id: Int)
 }
--- data/src/main/java/com/mpersand/data/remote/datasource/order/OrderDataSourceImpl.kt
+++ data/src/main/java/com/mpersand/data/remote/datasource/order/OrderDataSourceImpl.kt
@@ -1,6 +1,8 @@
 package com.mpersand.data.remote.datasource.order
 
 import com.mpersand.data.dto.order.request.OrderRequest
+import com.mpersand.data.dto.order.response.OrderDetailListResponse
+import com.mpersand.data.dto.order.response.OrderListResponse
 import com.mpersand.data.dto.order.response.OrderResponse
 import com.mpersand.data.dto.order.response.WaitListResponse
 import com.mpersand.data.network.api.OrderApi
@@ -10,9 +12,44 @@ import javax.inject.Inject
 class OrderDataSourceImpl @Inject constructor(
     private val orderApi: OrderApi
 ): OrderDataSource {
-    override suspend fun getNoReturnStudents(): List<OrderResponse> = safeApiCall { orderApi.getNoReturnStudents() }
+    override suspend fun getSelfStateList(): List<OrderListResponse> = safeApiCall {
+        orderApi.getSelfStateList()
+    }
 
-    override suspend fun getWaitList(): List<WaitListResponse> = safeApiCall { orderApi.getWaitList() }
+    override suspend fun getNowRentalList(): List<OrderDetailListResponse> = safeApiCall {
+        orderApi.getNowRentalList()
+    }
 
-    override suspend fun requestResult(body: OrderRequest) = safeApiCall { orderApi.requestResult(body) }
+    override suspend fun getNoReturnList(): List<OrderDetailListResponse> = safeApiCall {
+        orderApi.getNoReturnList()
+    }
+
+    override suspend fun getWaitList(): List<OrderDetailListResponse> = safeApiCall {
+        orderApi.getWaitList()
+    }
+
+    override suspend fun postRental(id: Int, response: String) = safeApiCall {
+        orderApi.postRental(id, response)
+    }
+
+    override suspend fun postReturn(id: Int) = safeApiCall {
+        orderApi.postReturn(id)
+    }
+
+    override suspend fun postExtension(id: Int, response: String) = safeApiCall {
+        orderApi.postExtension(id, response)
+    }
+
+    override suspend fun postRentalCancel(id: Int) = safeApiCall {
+        orderApi.postRentalCancel(id)
+    }
+
+    override suspend fun acceptRequest(id: Int) = safeApiCall {
+        orderApi.acceptRequest(id)
+    }
+
+    override suspend fun rejectRequest(id: Int) = safeApiCall {
+        orderApi.rejectRequest(id)
+    }
+
 }
