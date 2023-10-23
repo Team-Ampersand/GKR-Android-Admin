5e1c5e3655cd84bca878fbf44b9147bc0220a767
Cjsghkd <pjhomg5530@naver.com>
2023-10-23 11:27:24 +0900

    :recycle: 바뀐 api에 맞춰서 usecase 수정

--- domain/src/main/java/com/mpersand/domain/usecase/order/PostRentalUseCase.kt
+++ domain/src/main/java/com/mpersand/domain/usecase/order/PostRentalUseCase.kt
@@ -3,10 +3,10 @@ package com.mpersand.domain.usecase.order
 import com.mpersand.domain.repository.OrderRepository
 import javax.inject.Inject
 
-class GetWaitListUseCase @Inject constructor(
+class PostRentalUseCase @Inject constructor(
     private val orderRepository: OrderRepository
 ) {
-    suspend operator fun invoke() = kotlin.runCatching {
-        orderRepository.getWaitList()
+    suspend operator fun invoke(id: Int, response: String) = kotlin.runCatching {
+        orderRepository.postRental(id, response)
     }
 }
