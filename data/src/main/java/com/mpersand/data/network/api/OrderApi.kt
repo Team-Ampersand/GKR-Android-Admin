4c5280379f64bc817a1baa6feb7e0ca35131ed4d
Cjsghkd <pjhomg5530@naver.com>
2023-10-23 11:17:08 +0900

    :recycle: 바뀐 api에 맞춰서 수정

--- data/src/main/java/com/mpersand/data/network/api/OrderApi.kt
+++ data/src/main/java/com/mpersand/data/network/api/OrderApi.kt
@@ -1,19 +1,59 @@
 package com.mpersand.data.network.api
 
-import com.mpersand.data.dto.order.request.OrderRequest
-import com.mpersand.data.dto.order.response.OrderResponse
-import com.mpersand.data.dto.order.response.WaitListResponse
+import com.mpersand.data.dto.order.response.OrderDetailListResponse
+import com.mpersand.data.dto.order.response.OrderListResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface OrderApi {
+
+    @GET("order/state")
+    suspend fun getSelfStateList(): List<OrderListResponse>
+
+    @GET("order/now")
+    suspend fun getNowRentalList(): List<OrderDetailListResponse>
+
     @GET("order/noreturn")
-    suspend fun getNoReturnStudents(): List<OrderResponse>
+    suspend fun getNoReturnList(): List<OrderDetailListResponse>
 
     @GET("order/wait")
-    suspend fun getWaitList(): List<WaitListResponse>
+    suspend fun getWaitList(): List<OrderDetailListResponse>
 
-    @POST("order")
-    suspend fun requestResult(@Body body: OrderRequest)
+    @POST("order/rental/{id}")
+    suspend fun postRental(
+        @Path("id") id: Int,
+        @Body response: String
+    )
+
+    @POST("order/return/{id}")
+    suspend fun postReturn(
+        @Path("id") id: Int
+    )
+
+    @POST("order/extension/{id}")
+    suspend fun postExtension(
+        @Path("id") id: Int,
+        @Body response: String
+    )
+
+    @POST("order/cancel/{id}")
+    suspend fun postRentalCancel(
+        @Path("id") id: Int
+    )
+
+    @PATCH("order/accept/{id}")
+    suspend fun acceptRequest(
+        @Path("id") id: Int
+    )
+
+    @PATCH("order/reject/{id}")
+    suspend fun rejectRequest(
+        @Path("id") id: Int
+    )
+
+//    @POST("order")
+//    suspend fun requestResult(@Body body: OrderRequest)
 }
