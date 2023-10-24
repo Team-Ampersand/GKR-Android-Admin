217a1bb05be91293ec313db8b15485cdfd7296db
hyeonseo <s21035@gsm.hs.kr>
2023-10-24 14:12:45 +0900

    :lipstick: :: `fillMaxSize()` 추가

--- presentation/src/main/java/com/mpersand/presentation/view/detail/DetailScreen.kt
+++ presentation/src/main/java/com/mpersand/presentation/view/detail/DetailScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -53,6 +54,7 @@ fun DetailScreen(
 
             Column(
                 modifier = modifier
+                    .fillMaxSize()
                     .background(Color(0xFFFAFAFA))
                     .verticalScroll(rememberScrollState())
                     .systemBarsPadding()
